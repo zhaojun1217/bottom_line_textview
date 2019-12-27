e505747e3daa2a9b1210785bd8351eddcc080586
zhaoj <1334561398@qq.com>
2019-12-27 20:19:45 +0800

    i更新第二版 添加两个属性
    Signed-off-by: zhaoj <1334561398@qq.com>

--- app/src/main/java/com/zhaoj/jitpack_demo/MainActivity.java
+++ app/src/main/java/com/zhaoj/jitpack_demo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.zhaoj.jitpack_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import com.zhaoj.bottom_line_textview.BottomLineTextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomLineTextView bltTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bltTest = findViewById(R.id.bltTest);
+
+        bltTest.setText("测试的文本ss");
+        bltTest.setLineColor(0xff00ff00);
+        bltTest.setShowLine(true);
+        bltTest.setTextSize(20);
+        bltTest.setLineSize(4);
+        bltTest.setTextBold(true);
+        bltTest.setTextColor(0xff00ff00);
     }
 }
--- bottom_line_textview/src/main/java/com/zhaoj/bottom_line_textview/BottomLineTextView.java
+++ bottom_line_textview/src/main/java/com/zhaoj/bottom_line_textview/BottomLineTextView.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +26,9 @@ public class BottomLineTextView extends LinearLayout {
     private int textColor = Color.BLACK;
     private int lineColor = Color.BLACK;
     private boolean showLine = true;
+    private boolean textBold = false;
     private int textSize = 15;
+    private int lineSize = 1;
 
     public BottomLineTextView(Context context) {
         super(context);
@@ -37,34 +41,95 @@ public class BottomLineTextView extends LinearLayout {
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.bottom_line_text);
         defaultText = ta.getString(R.styleable.bottom_line_text_bottomLineDefaultText);
         textSize = ta.getInteger(R.styleable.bottom_line_text_bottomLineTextSize, 15);
+        lineSize = ta.getInteger(R.styleable.bottom_line_text_bottomLineLineSize, 1);
         textColor = ta.getColor(R.styleable.bottom_line_text_bottomLineTextColor, Color.BLACK);
         lineColor = ta.getColor(R.styleable.bottom_line_text_bottomLineLineColor, Color.BLACK);
         showLine = ta.getBoolean(R.styleable.bottom_line_text_bottomLineShowLine, true);
+        textBold = ta.getBoolean(R.styleable.bottom_line_text_bottomLineTextBold, false);
         initView();
         initStyle();
         initEvent();
     }
 
+    /**
+     * 设置文本
+     *
+     * @param str 文本
+     */
     public void setText(String str) {
         tvText.setText(str);
     }
 
-    public void setTextColor(int color)
-    {
+    /**
+     * 设置文本颜色
+     *
+     * @param color 文本颜色
+     */
+    public void setTextColor(int color) {
         tvText.setTextColor(color);
     }
 
-    public void setVLineColor(int color)
-    {
+    /**
+     * 设置下划线颜色
+     *
+     * @param color 下划线颜色
+     */
+    public void setLineColor(int color) {
         vLineView.setBackgroundColor(color);
     }
 
+    /**
+     * 设置是否隐藏下划线
+     *
+     * @param setShowLine 是否隐藏下划线
+     */
+    public void setShowLine(boolean setShowLine) {
+        this.showLine = setShowLine;
+        vLineView.setVisibility(showLine ? VISIBLE : GONE);
+    }
+
+    /**
+     * 粗体设置
+     *
+     * @param setTextBold bool值 是否加粗文本
+     */
+    public void setTextBold(boolean setTextBold) {
+        this.textBold = setTextBold;
+        tvText.getPaint().setFakeBoldText(textBold);//加粗
+    }
+
+    /**
+     * 设置文本字号
+     *
+     * @param setTextSize 文本字号
+     */
+    public void setTextSize(int setTextSize) {
+        this.textSize = setTextSize;
+        tvText.setTextSize(textSize);
+    }
+
+    /**
+     * 设置下划线宽度
+     *
+     * @param setLineSize 下划线宽度
+     */
+    public void setLineSize(int setLineSize) {
+        this.lineSize = setLineSize;
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, dip2px(mContext, lineSize));
+        vLineView.setLayoutParams(params);
+    }
+
+
     private void initStyle() {
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, dip2px(mContext, lineSize));
         tvText.setText(defaultText);
         tvText.setTextColor(textColor);
         tvText.setTextSize(textSize);
+//        tvText.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));//加粗
+        tvText.getPaint().setFakeBoldText(textBold);//加粗
         vLineView.setVisibility(showLine ? VISIBLE : GONE);
         vLineView.setBackgroundColor(lineColor);
+        vLineView.setLayoutParams(params);
     }
 
     private void initEvent() {
@@ -79,5 +144,15 @@ public class BottomLineTextView extends LinearLayout {
         addView(view, lp);
     }
 
+    //dp转px
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    //px转dp
+    public static int px2dip(Context context, int pxValue) {
+        return ((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pxValue, context.getResources().getDisplayMetrics()));
+    }
 
 }
